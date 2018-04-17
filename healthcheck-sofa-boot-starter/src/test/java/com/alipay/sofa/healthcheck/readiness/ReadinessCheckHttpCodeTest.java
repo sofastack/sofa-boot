7b821da0496a1532e28c7d75b08bd24b228ccb41
Khotyn Huang <hting1@gmail.com>
2018-04-17 11:37:48 +0800

    Clean up code (#29)
    Remove unsed import and  class, fix typos.

--- healthcheck-sofa-boot-starter/src/test/java/com/alipay/sofa/healthcheck/readiness/ReadinessCheckHttpCodeTest.java
+++ healthcheck-sofa-boot-starter/src/test/java/com/alipay/sofa/healthcheck/readiness/ReadinessCheckHttpCodeTest.java
@@ -20,11 +20,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
