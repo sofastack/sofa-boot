5d3a4c683966be28af692d6fcf53ff1ef0fd5951
QilongZhang <qilong.zql@alibaba-inc.com>
2018-06-12 14:30:48 +0800

    code format

--- healthcheck-sofa-boot-starter/src/test/java/com/alipay/sofa/healthcheck/usercases/HealthEndpointDisableConfigurationTest.java
+++ healthcheck-sofa-boot-starter/src/test/java/com/alipay/sofa/healthcheck/usercases/HealthEndpointDisableConfigurationTest.java
@@ -1,6 +1,18 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2018 All Rights Reserved.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alipay.sofa.healthcheck.usercases;
 
@@ -19,7 +31,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @since 2.5.0
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = SofaBootTestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = {"com.alipay.sofa.healthcheck.readiness.enabled=false"})
+@SpringBootTest(classes = SofaBootTestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = { "com.alipay.sofa.healthcheck.readiness.enabled=false" })
 public class HealthEndpointDisableConfigurationTest {
 
     @Autowired
@@ -27,10 +39,12 @@ public class HealthEndpointDisableConfigurationTest {
 
     @Test
     public void test() {
-        SofaBootReadinessCheckEndpoint sofaBootReadinessCheckEndpoint = (SofaBootReadinessCheckEndpoint)ctx.getBean("readinessCheck");
+        SofaBootReadinessCheckEndpoint sofaBootReadinessCheckEndpoint = (SofaBootReadinessCheckEndpoint) ctx
+            .getBean("readinessCheck");
         Assert.assertFalse(sofaBootReadinessCheckEndpoint.isEnabled());
 
-        boolean sofaBootVersionEndpointMvcAdapter = ctx.containsBean("sofaBootReadinessCheckMvcEndpoint");
+        boolean sofaBootVersionEndpointMvcAdapter = ctx
+            .containsBean("sofaBootReadinessCheckMvcEndpoint");
         Assert.assertFalse(sofaBootVersionEndpointMvcAdapter);
     }
 
