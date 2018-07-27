b85bcf7170d35bbca3df5314c4046ed077dc807c
QilongZhang <qilong.zql@alibaba-inc.com>
2018-07-27 11:52:50 +0800

    re merger

--- healthcheck-sofa-boot-starter/src/main/java/com/alipay/sofa/healthcheck/core/DefaultHealthChecker.java
+++ healthcheck-sofa-boot-starter/src/main/java/com/alipay/sofa/healthcheck/core/DefaultHealthChecker.java
@@ -19,7 +19,7 @@ package com.alipay.sofa.healthcheck.core;
 /**
  * @author liangen
  * @version 2.3.0
- * @deprecated this class is not intended for use and will be removed the next major versions.
+ * @deprecated this class is not intended for use and will be removed the next major version.
  * {@link org.springframework.boot.actuate.health.HealthIndicator} is recommended.
  */
 @Deprecated
