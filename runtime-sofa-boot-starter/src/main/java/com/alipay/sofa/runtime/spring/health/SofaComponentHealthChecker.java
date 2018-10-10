f8c7e2874410c3d5bdafe780b896b97f1e695a79
straybird <abby.zh@antfin.com>
2018-10-10 21:56:40 +0800

    cherry pick 2.4.10

--- runtime-sofa-boot-starter/src/main/java/com/alipay/sofa/runtime/spring/health/SofaComponentHealthChecker.java
+++ runtime-sofa-boot-starter/src/main/java/com/alipay/sofa/runtime/spring/health/SofaComponentHealthChecker.java
@@ -16,8 +16,10 @@
  */
 package com.alipay.sofa.runtime.spring.health;
 
+import com.alipay.sofa.healthcheck.configuration.HealthCheckConfigurationConstants;
 import com.alipay.sofa.healthcheck.core.HealthChecker;
 import com.alipay.sofa.runtime.spi.component.SofaRuntimeContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 
 /**
@@ -27,6 +29,16 @@ import org.springframework.boot.actuate.health.Health;
  */
 public class SofaComponentHealthChecker extends AbstractComponentHealthChecker implements
                                                                               HealthChecker {
+
+    @Value("${" + HealthCheckConfigurationConstants.SOFABOOT_COMPONENT_CHECK_RETRY_COUNT + ":"
+           + HealthCheckConfigurationConstants.SOFABOOT_COMPONENT_CHECK_RETRY_DEFAULT_COUNT + "}")
+    private int retryCount;
+
+    @Value("${" + HealthCheckConfigurationConstants.SOFABOOT_COMPONENT_CHECK_RETRY_INTERVAL + ":"
+           + HealthCheckConfigurationConstants.SOFABOOT_COMPONENT_CHECK_RETRY_DEFAULT_INTERVAL
+           + "}")
+    private int retryInterval;
+
     public SofaComponentHealthChecker(SofaRuntimeContext sofaRuntimeContext) {
         super(sofaRuntimeContext);
     }
@@ -43,12 +55,12 @@ public class SofaComponentHealthChecker extends AbstractComponentHealthChecker i
 
     @Override
     public int getRetryCount() {
-        return 0;
+        return retryCount;
     }
 
     @Override
     public long getRetryTimeInterval() {
-        return 0;
+        return retryInterval;
     }
 
     @Override
