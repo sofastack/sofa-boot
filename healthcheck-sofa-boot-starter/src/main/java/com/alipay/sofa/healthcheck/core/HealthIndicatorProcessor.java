e348ce11fc542faf10d6fbdd42609602dc4f2e3a
QilongZhang <qilong.zql@alibaba-inc.com>
2018-10-23 13:02:49 +0800

    fix  as review

--- healthcheck-sofa-boot-starter/src/main/java/com/alipay/sofa/healthcheck/core/HealthIndicatorProcessor.java
+++ healthcheck-sofa-boot-starter/src/main/java/com/alipay/sofa/healthcheck/core/HealthIndicatorProcessor.java
@@ -52,12 +52,14 @@ public class HealthIndicatorProcessor {
     @Autowired
     private ApplicationContext                     applicationContext;
 
+    private final static String                    REACTOR_CLASS    = "reactor.core.publisher.Mono";
+
     public void init() {
         if (isInitiated.compareAndSet(false, true)) {
             Assert.notNull(applicationContext, "Application must not be null");
             Map<String, HealthIndicator> beansOfType = applicationContext
                 .getBeansOfType(HealthIndicator.class);
-            if (ClassUtils.isPresent("reactor.core.publisher.Flux", null)) {
+            if (ClassUtils.isPresent(REACTOR_CLASS, null)) {
                 applicationContext.getBeansOfType(ReactiveHealthIndicator.class).forEach((name, indicator)->beansOfType.put(name, ()->indicator.health().block()));
             }
             healthIndicators = HealthCheckUtils.sortMapAccordingToValue(beansOfType,
