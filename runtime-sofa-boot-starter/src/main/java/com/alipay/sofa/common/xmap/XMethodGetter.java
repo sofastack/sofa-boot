2ed1cfdef715ac015d329e971c3a9f34bb48e1dd
straybird <abby.zh@antfin.com>
2019-01-23 20:49:14 +0800

    add extension and extension point support (#340)

---
+++ runtime-sofa-boot-starter/src/main/java/com/alipay/sofa/common/xmap/XMethodGetter.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.common.xmap;
+
+import java.lang.reflect.Method;
+
+/**
+ *
+ * @author xi.hux@alipay.com
+ * @since 2.6.0
+ */
+public class XMethodGetter implements XGetter {
+
+    /**
+     * method
+     */
+    private Method   method;
+
+    /**
+     * class
+     */
+    private Class<?> clazz;
+
+    /**
+     * bean name
+     */
+    private String   name;
+
+    public XMethodGetter(Method method, Class<?> clazz, String name) {
+        this.method = method;
+        this.clazz = clazz;
+        this.name = name;
+    }
+
+    /**
+     * @see XGetter#getType()
+     */
+    public Class<?> getType() {
+        if (method == null) {
+            throw new IllegalArgumentException("no such getter method: " + clazz.getName() + '.'
+                                               + name);
+        }
+
+        return method.getReturnType();
+    }
+
+    /**
+     * @see XGetter#getValue(Object)
+     */
+    public Object getValue(Object instance) throws Exception {
+        if (method == null) {
+            throw new IllegalArgumentException("no such getter method: " + clazz.getName() + '.'
+                                               + name);
+        }
+
+        if (instance == null) {
+            return null;
+        }
+        return method.invoke(instance, new Object[0]);
+    }
+
+}
--- runtime-sofa-boot-starter/src/test/java/com/alipay/sofa/runtime/integration/postprocessor/BeanPostProcessorOrderTest.java
+++ runtime-sofa-boot-starter/src/test/java/com/alipay/sofa/runtime/integration/postprocessor/BeanPostProcessorOrderTest.java
@@ -17,7 +17,7 @@
 package com.alipay.sofa.runtime.integration.postprocessor;
 
 import com.alipay.sofa.runtime.api.event.ApplicationShutdownCallback;
-import com.alipay.sofa.runtime.integration.base.SofaBootTestApplication;
+import com.alipay.sofa.runtime.integration.bootstrap.SofaBootTestApplication;
 import com.alipay.sofa.runtime.spi.component.SofaRuntimeContext;
 import com.alipay.sofa.runtime.spi.component.SofaRuntimeManager;
 import org.junit.Assert;
