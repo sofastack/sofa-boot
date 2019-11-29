18f3518012790380d732834ce17f8381be008301
纶珥 <guaner.zzx@alibaba-inc.com>
2019-11-29 12:36:38 +0800

    component health check result display binding type

--- sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/service/component/ServiceComponent.java
+++ sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/service/component/ServiceComponent.java
@@ -158,6 +158,7 @@ public class ServiceComponent extends AbstractComponent {
                         getContext());
                 } catch (Throwable t) {
                     allPassed = false;
+                    binding.setHealthy(false);
                     SofaLogger.error(t, " <<Out binding [{0}] for [{1}] occur exception.",
                         binding.getBindingType(), service);
                     continue;
@@ -306,17 +307,14 @@ public class ServiceComponent extends AbstractComponent {
 
     @Override
     public HealthResult isHealthy() {
-        if (!isActivated()) {
-            return super.isHealthy();
-        }
-
         HealthResult healthResult = new HealthResult(componentName.getRawName());
         if (this.e == null) {
             healthResult.setHealthy(true);
         } else {
             healthResult.setHealthy(false);
-            healthResult.setHealthReport(e.getMessage());
         }
+        healthResult.setHealthReport(aggregateBindingHealth(service.getBindings()));
+
         return healthResult;
     }
 }
