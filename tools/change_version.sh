3eeabc6388bc86b704c166dc6ff13b33dd99acf3
纶珥 <guaner.zzx@alibaba-inc.com>
2019-12-18 14:00:21 +0800

    fix change_version.sh bug

--- tools/change_version.sh
+++ tools/change_version.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 shopt -s expand_aliases
 if [ ! -n "$1" ] ;then
-	echo "Please enter a version"
+	echo "Please input a version as first argument, e.g., 3.2.2"
  	exit 1
 else
-  	echo "The updated version is $1 !"
+  	echo "Version to update is $1"
 fi
 
 currentVersion=`sed -n '/<project /,/<packaging/p' pom.xml | grep version | cut -d '>' -f2 | cut -d '<' -f1`
-echo "The current version is $currentVersion"
+echo "Current version is $currentVersion"
 
 if [ `uname` == "Darwin" ] ;then
- 	echo "This is OS X"
  	alias sed='sed -i ""'
 else
- 	echo "This is Linux"
  	alias sed='sed -i'
 fi
 
@@ -22,14 +20,13 @@ echo "Change version in root pom.xml ===>"
 sed "/<project /,/<packaging/ s/<version>.*<\/version>/<version>$1<\/version>/" pom.xml
 
 echo "Change version in subproject pom ===>"
-for filename in `find . -name "pom.xml" -mindepth 2`;do
-    echo "Deal with $filename"
+for filename in $(find ./sofa-boot-project ! -path '*/sofaboot-dependencies/*' -name "pom.xml"); do
+    echo "Dealing with sofa-boot-project${filename:1}"
     sed "/<parent>/,/<\/parent>/ s/<version>.*<\/version>/<version>$1<\/version>/" $filename
 done
 
 echo "Change version in gradle.properties"
-for filename in `find . -name "gradle.properties" -mindepth 3`;do
-    echo "Deal with $filename"
+for filename in $(find . -name "gradle.properties" -mindepth 3); do
+    echo "Dealing with $filename"
     sed "s/sofaVersion=.*/sofaVersion=$1/g" $filename
 done
-
