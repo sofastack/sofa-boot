e51e158a7f317809852f2f417cb9ed19e81fc4c3
纶珥 <guaner.zzx@alibaba-inc.com>
2020-03-09 11:30:51 +0800

    fix MultiApplicationHealthIndicator bug

--- sofa-boot-project/sofa-boot-core/healthcheck-sofa-boot/src/main/java/com/alipay/sofa/healthcheck/impl/SofaRuntimeHealthChecker.java
+++ sofa-boot-project/sofa-boot-core/healthcheck-sofa-boot/src/main/java/com/alipay/sofa/healthcheck/impl/SofaRuntimeHealthChecker.java
@@ -49,6 +49,10 @@ public class SofaRuntimeHealthChecker implements RuntimeHealthChecker {
     @Override
     public boolean isLivenessHealth() {
         for (HealthIndicator healthIndicator : healthIndicators) {
+            if (healthIndicator.getClass().getSimpleName()
+                .equals("MultiApplicationHealthIndicator")) {
+                continue;
+            }
             if (healthIndicator.health().getStatus().equals(Status.DOWN)) {
                 return false;
             }
