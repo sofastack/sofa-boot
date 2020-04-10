b233f90fd43be58fda25f2860ba7795d14a694bb
leizhiyuan <leizhiyuan@gmail.com>
2020-04-10 17:45:00 +0800

    feat:default is protobuf

--- sofa-boot-project/sofa-boot-core/rpc-sofa-boot/src/main/java/com/alipay/sofa/rpc/boot/runtime/converter/TripleBindingConverter.java
+++ sofa-boot-project/sofa-boot-core/rpc-sofa-boot/src/main/java/com/alipay/sofa/rpc/boot/runtime/converter/TripleBindingConverter.java
@@ -17,11 +17,14 @@
 package com.alipay.sofa.rpc.boot.runtime.converter;
 
 import com.alipay.sofa.rpc.boot.config.SofaBootRpcConfigConstants;
+import com.alipay.sofa.rpc.boot.runtime.binding.RpcBindingXmlConstants;
 import com.alipay.sofa.rpc.boot.runtime.binding.TripleBinding;
 import com.alipay.sofa.rpc.boot.runtime.binding.RpcBinding;
 import com.alipay.sofa.rpc.boot.runtime.binding.RpcBindingType;
 import com.alipay.sofa.rpc.boot.runtime.param.TripleBindingParam;
 import com.alipay.sofa.rpc.boot.runtime.param.RpcBindingParam;
+import com.alipay.sofa.rpc.common.RpcConstants;
+import com.alipay.sofa.rpc.common.utils.StringUtils;
 import com.alipay.sofa.runtime.api.annotation.SofaReference;
 import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
 import com.alipay.sofa.runtime.api.annotation.SofaService;
@@ -29,6 +32,7 @@ import com.alipay.sofa.runtime.api.annotation.SofaServiceBinding;
 import com.alipay.sofa.runtime.api.binding.BindingType;
 import com.alipay.sofa.runtime.spi.service.BindingConverterContext;
 import org.springframework.context.ApplicationContext;
+import org.w3c.dom.Element;
 
 /**
  * @author <a href="mailto:zhiyuan.lzy@antfin.com">zhiyuan.lzy</a>
@@ -66,6 +70,17 @@ public class TripleBindingConverter extends RpcBindingConverter {
 
         return new TripleBinding(bindingParam, bindingConverterContext.getApplicationContext(),
             bindingConverterContext.isInBinding());
+    }
+
+    @Override
+    protected void parseGlobalAttrs(Element element, RpcBindingParam param,
+                                    BindingConverterContext bindingConverterContext) {
+        super.parseGlobalAttrs(element, param, bindingConverterContext);
+        String serialization = element.getAttribute(RpcBindingXmlConstants.TAG_SERIALIZE_TYPE);
+        if (StringUtils.isBlank(serialization)) {
+            param.setSerialization(RpcConstants.SERIALIZE_PROTOBUF);
+        }
+
     }
 
     @Override
