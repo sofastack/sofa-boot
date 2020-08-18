8e162c7ffd1fc644a8d1dd7e68f6dcd8e9146736
纶珥 <guaner.zzx@alibaba-inc.com>
2020-08-18 21:43:07 +0800

    add jvm filter tests

---
+++ sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/ambush/EgressFilter.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.runtime.ambush;
+
+import org.springframework.core.Ordered;
+
+/**
+ * @author <a href="mailto:guaner.zzx@alipay.com">Alaneuler</a>
+ * Created on 2020/8/18
+ */
+public interface EgressFilter extends Ordered {
+    /**
+     * This method is called after the actual JVM service invoking.
+     * Filter can replace the <code>invokeResult</code> of context to do something nasty.
+     * @param context JVM invoking context
+     * @return whether to continue processing
+     */
+    boolean after(Context context);
+}
---
+++ sofa-boot-project/sofa-boot-starters/runtime-sofa-boot-starter/src/test/java/com/alipay/sofa/runtime/test/ambush/JvmFilterInterruptedConfiguration.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.runtime.test.ambush;
+
+import com.alipay.sofa.runtime.ambush.Context;
+import com.alipay.sofa.runtime.ambush.IngressFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * @author <a href="mailto:guaner.zzx@alipay.com">Alaneuler</a>
+ * Created on 2020/8/18
+ */
+@EnableAutoConfiguration
+@ImportResource("classpath:spring/runtime.xml")
+public class JvmFilterInterruptedConfiguration {
+    public static int beforeCount = 0;
+    public static int afterCount  = 0;
+
+    @Bean
+    public IngressFilter ingressFilter1() {
+        return new IngressFilter() {
+            @Override
+            public boolean before(Context context) {
+                ++beforeCount;
+                return true;
+            }
+
+            @Override
+            public int getOrder() {
+                return 0;
+            }
+        };
+    }
+
+    @Bean
+    public IngressFilter ingressFilter2() {
+        return new IngressFilter() {
+            @Override
+            public boolean before(Context context) {
+                ++beforeCount;
+                return false;
+            }
+
+            @Override
+            public int getOrder() {
+                return 100;
+            }
+        };
+    }
+
+    @Bean
+    public IngressFilter ingressFilter3() {
+        return new IngressFilter() {
+            @Override
+            public boolean before(Context context) {
+                ++beforeCount;
+                context.setInvokeResult("interrupted");
+                return false;
+            }
+
+            @Override
+            public int getOrder() {
+                return -100;
+            }
+        };
+    }
+}
