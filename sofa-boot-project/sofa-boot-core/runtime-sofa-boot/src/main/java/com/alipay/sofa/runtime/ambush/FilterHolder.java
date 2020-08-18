fc18cbb517b03ba891854d1ef98c8e95bbf71db5
纶珥 <guaner.zzx@alibaba-inc.com>
2020-08-18 15:19:58 +0800

    format

--- sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/ambush/FilterHolder.java
+++ sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/ambush/FilterHolder.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.runtime.ambush;
 
 import java.util.ArrayList;
@@ -10,8 +26,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * Created on 2020/8/18
  */
 public class FilterHolder {
-    private static final List<Filter> filters = new ArrayList<>();
-    private static final AtomicBoolean sorted = new AtomicBoolean(false);
+    private static final List<Filter>  filters = new ArrayList<>();
+    private static final AtomicBoolean sorted  = new AtomicBoolean(false);
 
     public static void addFilter(Filter filter) {
         filters.add(filter);
@@ -38,14 +54,14 @@ public class FilterHolder {
 
     public static void beforeInvoking(Context context) {
         sortFilters();
-        for (Filter filter: filters) {
+        for (Filter filter : filters) {
             filter.before(context);
         }
     }
 
     public static void afterInvoking(Context context) {
         sortFilters();
-        for (Filter filter: filters) {
+        for (Filter filter : filters) {
             filter.after(context);
         }
     }
--- sofa-boot-project/sofa-boot-starters/runtime-sofa-boot-starter/src/test/java/com/alipay/sofa/runtime/test/ambush/JvmFilterTestConfiguration.java
+++ sofa-boot-project/sofa-boot-starters/runtime-sofa-boot-starter/src/test/java/com/alipay/sofa/runtime/test/ambush/JvmFilterTestConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.runtime.test.ambush;
 
 import com.alipay.sofa.runtime.ambush.Context;
@@ -14,7 +30,7 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath:spring/runtime.xml")
 public class JvmFilterTestConfiguration {
     public static int beforeCount = 0;
-    public static int afterCount = 0;
+    public static int afterCount  = 0;
 
     @Bean
     public Filter filter1() {
