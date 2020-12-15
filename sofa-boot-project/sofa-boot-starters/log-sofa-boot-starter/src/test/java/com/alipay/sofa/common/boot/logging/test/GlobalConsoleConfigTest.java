00824126a58c7184cbf3306bd57320058f4f25c0
alaneuler <alaneuler@qq.com>
2020-12-16 00:34:41 +0800

    fix log starter test

---
+++ sofa-boot-project/sofa-boot-starters/log-sofa-boot-starter/src/test/java/com/alipay/sofa/common/boot/logging/test/GlobalConsoleConfigTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.common.boot.logging.test;
+
+import com.alipay.sofa.common.log.Constants;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:guaner.zzx@alipay.com">Alaneuler</a>
+ * Created on 2020/12/16
+ */
+public class GlobalConsoleConfigTest extends LogTestBase {
+    /**
+     * test sofa.middleware.log.console config
+     */
+    @Test
+    public void test() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(Constants.SOFA_MIDDLEWARE_ALL_LOG_CONSOLE_SWITCH, "true");
+        SpringApplication springApplication = new SpringApplication(EmptyConfig.class);
+        springApplication.setDefaultProperties(properties);
+        springApplication.run();
+        logger = getLogger();
+        logger.info("global space console");
+        logger.debug("global space console debug");
+        Assert.assertTrue(outContent.toString().contains("global space console"));
+        Assert.assertFalse(outContent.toString().contains("global space console debug"));
+    }
+}
---
+++ sofa-boot-project/sofa-boot-starters/log-sofa-boot-starter/src/test/java/com/alipay/sofa/common/boot/logging/test/SpaceConsoleConfigTest.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.common.boot.logging.test;
+
+import com.alipay.sofa.common.log.Constants;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:guaner.zzx@alipay.com">Alaneuler</a>
+ * Created on 2020/12/16
+ */
+public class SpaceConsoleConfigTest extends LogTestBase {
+    /**
+     * test sofa.middleware.log.{space id}.console config
+     */
+    @Test
+    public void testSpaceConsoleConfig() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(
+            String.format(Constants.SOFA_MIDDLEWARE_SINGLE_LOG_CONSOLE_SWITCH, TEST_SPACE), "true");
+        SpringApplication springApplication = new SpringApplication(EmptyConfig.class);
+        springApplication.setDefaultProperties(properties);
+        springApplication.run();
+        logger = getLogger();
+        logger.info("space console");
+        logger.debug("space console debug");
+        Assert.assertTrue(outContent.toString().contains("space console"));
+        Assert.assertFalse(outContent.toString().contains("space console debug"));
+    }
+}
