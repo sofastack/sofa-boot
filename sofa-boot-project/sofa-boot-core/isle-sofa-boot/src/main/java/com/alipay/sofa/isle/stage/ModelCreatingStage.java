0aa099f5acaa799c5f9bf5e2118933709a9da02b
HzjNeverStop <441627022@qq.com>
2021-12-23 14:57:11 +0800

    Throw exception when found duplicate module (#888)
    * throw exception for duplicate module
    
    Co-authored-by: 致节 <hzj266771@antgroup.com>

--- sofa-boot-project/sofa-boot-core/isle-sofa-boot/src/main/java/com/alipay/sofa/isle/stage/ModelCreatingStage.java
+++ sofa-boot-project/sofa-boot-core/isle-sofa-boot/src/main/java/com/alipay/sofa/isle/stage/ModelCreatingStage.java
@@ -16,24 +16,27 @@
  */
 package com.alipay.sofa.isle.stage;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Properties;
-
+import com.alipay.sofa.boot.constant.SofaBootConstants;
+import com.alipay.sofa.boot.error.ErrorCode;
+import com.alipay.sofa.isle.ApplicationRuntimeModel;
+import com.alipay.sofa.isle.deployment.DeploymentBuilder;
+import com.alipay.sofa.isle.deployment.DeploymentDescriptor;
+import com.alipay.sofa.isle.deployment.DeploymentDescriptorConfiguration;
+import com.alipay.sofa.isle.deployment.DeploymentException;
+import com.alipay.sofa.isle.deployment.impl.DefaultModuleDeploymentValidator;
+import com.alipay.sofa.isle.profile.SofaModuleProfileChecker;
+import com.alipay.sofa.isle.spring.config.SofaModuleProperties;
+import com.alipay.sofa.runtime.log.SofaLogger;
 import com.alipay.sofa.runtime.spi.component.SofaRuntimeManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.core.io.UrlResource;
 
-import com.alipay.sofa.boot.constant.SofaBootConstants;
-import com.alipay.sofa.isle.ApplicationRuntimeModel;
-import com.alipay.sofa.isle.deployment.DeploymentBuilder;
-import com.alipay.sofa.isle.deployment.DeploymentDescriptor;
-import com.alipay.sofa.isle.deployment.DeploymentDescriptorConfiguration;
-import com.alipay.sofa.isle.deployment.impl.DefaultModuleDeploymentValidator;
-import com.alipay.sofa.isle.profile.SofaModuleProfileChecker;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
 
 /**
  *
@@ -42,14 +45,19 @@ import com.alipay.sofa.isle.profile.SofaModuleProfileChecker;
  * @version $Id: ModelCreatingStage.java, v 0.1 2012-3-16 14:17:48 fengqi.lin Exp $
  */
 public class ModelCreatingStage extends AbstractPipelineStage {
+    private final boolean              allowModuleOverriding;
 
     @Autowired
     protected SofaModuleProfileChecker sofaModuleProfileChecker;
 
     public ModelCreatingStage(AbstractApplicationContext applicationContext) {
         super(applicationContext);
+        SofaModuleProperties sofaModuleProperties = applicationContext
+            .getBean(SofaModuleProperties.class);
+        this.allowModuleOverriding = sofaModuleProperties.isAllowModuleOverriding();
     }
 
+    @Override
     protected void doProcess() throws Exception {
         ApplicationRuntimeModel application = new ApplicationRuntimeModel();
         application.setAppName(appName);
@@ -64,7 +72,8 @@ public class ModelCreatingStage extends AbstractPipelineStage {
             application);
     }
 
-    protected void getAllDeployments(ApplicationRuntimeModel application) throws IOException {
+    protected void getAllDeployments(ApplicationRuntimeModel application) throws IOException,
+                                                                         DeploymentException {
         Enumeration<URL> urls = appClassLoader.getResources(SofaBootConstants.SOFA_MODULE_FILE);
         if (urls == null || !urls.hasMoreElements()) {
             return;
@@ -83,12 +92,30 @@ public class ModelCreatingStage extends AbstractPipelineStage {
 
             if (application.isModuleDeployment(dd)) {
                 if (sofaModuleProfileChecker.acceptModule(dd)) {
-                    application.addDeployment(dd);
+                    validateDuplicateModule(application.addDeployment(dd), dd);
                 } else {
                     application.addInactiveDeployment(dd);
                 }
             }
         }
+    }
+
+    protected void validateDuplicateModule(DeploymentDescriptor exist, DeploymentDescriptor dd)
+                                                                                               throws DeploymentException {
+        if (exist != null) {
+            if (isAllowModuleOverriding()) {
+                SofaLogger.warn("Overriding module deployment for module name '"
+                                + dd.getModuleName() + "': replacing '" + exist.getName()
+                                + "' with '" + dd.getName() + "'");
+            } else {
+                throw new DeploymentException(ErrorCode.convert("01-11006", dd.getModuleName(),
+                    exist.getName(), dd.getName()));
+            }
+        }
+    }
+
+    protected boolean isAllowModuleOverriding() {
+        return this.allowModuleOverriding;
     }
 
     @Override
