7d479f6d605f40433e48ceeb27123784b1dd49f3
致节 <hzj266771@antgroup.com>
2022-05-26 16:21:50 +0800

    add JvmBindingInterface in sofa reference proxy

--- sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/service/binding/JvmBindingAdapter.java
+++ sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/service/binding/JvmBindingAdapter.java
@@ -124,11 +124,11 @@ public class JvmBindingAdapter implements BindingAdapter<JvmBinding> {
             ProxyFactory factory = new ProxyFactory();
             if (javaClass.isInterface()) {
                 factory.addInterface(javaClass);
+                factory.addInterface(JvmBindingInterface.class);
             } else {
                 factory.setTargetClass(javaClass);
                 factory.setProxyTargetClass(true);
             }
-            factory.addInterface(JvmBindingInterface.class);
             factory.addAdvice(handler);
             return factory.getProxy(newClassLoader);
         } finally {
