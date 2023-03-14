c8fb72edb5c2afadc0feaff50a5c346a793b2689
致节 <hzj266771@antgroup.com>
2023-03-14 18:08:18 +0800

    support custom readiness actuator http response code

---
+++ sofa-boot-tests/sofa-boot-smoke-tests/sofa-boot-smoke-tests-actuator/src/test/java/com/alipay/sofa/smoke/tests/actuator/health/SofaHttpCodeStatusMapperTests.java
@@ -0,0 +1,94 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.smoke.tests.actuator.health;
+
+import com.alipay.sofa.boot.actuator.health.HealthChecker;
+import com.alipay.sofa.smoke.tests.actuator.ActuatorSofaBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Integration tests for {@link SofaHttpCodeStatusMapperTests} web response.
+ *
+ * @author huzijie
+ * @version SofaHttpCodeStatusMapperTests.java, v 0.1 2023年03月14日 5:53 PM huzijie Exp $
+ */
+@SpringBootTest(classes = ActuatorSofaBootApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
+                                                                                                                                       "management.endpoints.web.exposure.include=readiness",
+                                                                                                                                       "sofa.boot.actuator.health.status.http-mapping.down=510" })
+@Import(SofaHttpCodeStatusMapperTests.ErrorHealthChecker.class)
+public class SofaHttpCodeStatusMapperTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void health() {
+        ResponseEntity<SofaHttpCodeStatusMapperTests.HealthResponse> response = restTemplate
+            .getForEntity("/actuator/readiness", SofaHttpCodeStatusMapperTests.HealthResponse.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_EXTENDED);
+        SofaHttpCodeStatusMapperTests.HealthResponse health = response.getBody();
+        assertThat(health).isNotNull();
+        assertThat(health.getDetails().toString()).contains("ErrorHealthChecker");
+    }
+
+    static class ErrorHealthChecker implements HealthChecker {
+
+        @Override
+        public Health isHealthy() {
+            return Health.down().build();
+        }
+
+        @Override
+        public String getComponentName() {
+            return "errorComponent";
+        }
+    }
+
+    private static class HealthResponse {
+
+        private String              status;
+
+        private Map<String, Object> details;
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public Map<String, Object> getDetails() {
+            return details;
+        }
+
+        public void setDetails(Map<String, Object> details) {
+            this.details = details;
+        }
+    }
+}
