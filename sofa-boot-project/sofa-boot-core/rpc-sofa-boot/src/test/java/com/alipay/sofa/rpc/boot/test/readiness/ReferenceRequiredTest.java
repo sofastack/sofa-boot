d326ca99508a4ed96ea47df8559841dae2b76be7
寻芳 <dengjialin.djl@antgroup.com>
2023-03-29 16:39:52 +0800

    Add required property for SofaReference, including annotation/xml/api registration.
    If required=false, health check will still pass even if there is no corresponding service.

--- sofa-boot-project/sofa-boot-core/rpc-sofa-boot/src/test/java/com/alipay/sofa/rpc/boot/test/readiness/ReferenceRequiredTest.java
+++ sofa-boot-project/sofa-boot-core/rpc-sofa-boot/src/test/java/com/alipay/sofa/rpc/boot/test/readiness/ReferenceRequiredTest.java
@@ -1,6 +1,18 @@
 /*
- * Ant Group
- * Copyright (c) 2004-2023 All Rights Reserved.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alipay.sofa.rpc.boot.test.readiness;
 
@@ -45,24 +57,26 @@ public class ReferenceRequiredTest {
     private ApplicationContext applicationContext;
 
     @Autowired
-    private ClientFactoryBean clientFactoryBean;
+    private ClientFactoryBean  clientFactoryBean;
 
     @SofaReference(uniqueId = "requireServiceAnnotation", required = false)
-    private RequireService requireServiceAnnotation;
+    private RequireService     requireServiceAnnotation;
 
     @PostConstruct
     public void init() {
-        ReferenceClient referenceClient = clientFactoryBean.getClientFactory().getClient(ReferenceClient.class);
+        ReferenceClient referenceClient = clientFactoryBean.getClientFactory().getClient(
+            ReferenceClient.class);
         ReferenceParam<RequireService> referenceParam = new ReferenceParam<>();
         referenceParam.setInterfaceType(RequireService.class);
         referenceParam.setUniqueId("requireServiceClient");
         referenceParam.setRequired(false);
-        RequireService requireServiceClient = referenceClient.reference(referenceParam);
+        referenceClient.reference(referenceParam);
     }
 
     @Test
     public void testHealthCheckerConfig() {
-        HealthCheckerProcessor healthCheckerProcessor = applicationContext.getBean(HealthCheckerProcessor.class);
+        HealthCheckerProcessor healthCheckerProcessor = applicationContext
+            .getBean(HealthCheckerProcessor.class);
         Map<String, Health> healthMap = new HashMap<>();
         boolean result = healthCheckerProcessor.readinessHealthCheck(healthMap);
         Assert.assertTrue(result);
