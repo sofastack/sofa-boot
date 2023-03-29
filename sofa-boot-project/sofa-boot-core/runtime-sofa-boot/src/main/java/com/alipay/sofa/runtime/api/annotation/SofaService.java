ed04768c571974ce65c73bbd559f8d2655030bd1
HzjNeverStop <441627022@qq.com>
2023-03-29 10:14:19 +0800

    support repeatable @SofaServices annotation (#1132)
    Co-authored-by: 致节 <hzj266771@antgroup.com>

--- sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/api/annotation/SofaService.java
+++ sofa-boot-project/sofa-boot-core/runtime-sofa-boot/src/main/java/com/alipay/sofa/runtime/api/annotation/SofaService.java
@@ -17,6 +17,7 @@
 package com.alipay.sofa.runtime.api.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -40,6 +41,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.METHOD })
+@Repeatable(SofaServices.class)
 public @interface SofaService {
 
     /**
