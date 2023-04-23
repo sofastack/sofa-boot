4d6e52ac206b57aa8db3639b6f7f699a6d18cb24
致节 <hzj266771@antgroup.com>
2023-04-23 17:08:35 +0800

    remove unused import

--- sofa-boot-project/sofa-boot-autoconfigure/src/test/java/com/alipay/sofa/boot/autoconfigure/tracer/SofaTracerConfigurationListenerTests.java
+++ sofa-boot-project/sofa-boot-autoconfigure/src/test/java/com/alipay/sofa/boot/autoconfigure/tracer/SofaTracerConfigurationListenerTests.java
@@ -31,7 +31,6 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
