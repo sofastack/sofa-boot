b8eedc60a3eb4e197b90869763c0156b9f92c2e2
长霁 <pym328316@alibaba-inc.com>
2023-08-08 20:49:17 +0800

    feat: Added side-effect-free stubbing framework for SOFABoot applications
    With the support of @SofaMockBeanFor and @SofaSpyBeanFor annotations, you can easily mock/spy external dependencies when writing integration tests. More importantly, these annotations does not produce any side-effects, which means that the Spring ApplicationContext can be efficiently cached and reused among different test cases, thus greatly reduce test cost.

---
+++ sofa-boot-project/sofa-boot-core/test-sofa-boot/src/main/java/com/alipay/sofa/testing/api/annotation/SofaSpyBeanFor.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.testing.api.annotation;
+
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.testing.api.annotation.SofaSpyBeanFor.SofaSpyBeans;
+import org.mockito.Spy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Similar to @{@link SofaMockBeanFor}
+ *
+ * @author pengym
+ * @version SofaSpyBeanFor.java, v 0.1 2023年08月07日 15:38 pengym
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(SofaSpyBeans.class)
+public @interface SofaSpyBeanFor {
+    /**
+     * This parameter specifies the target {@link Class} to be injected with a {@link Spy} instance during test execution. The targets can be regular Spring {@link Bean}s or {@link SofaReference} objects. Note that if multiple targets are matched, they will <b>all</b> be injected with the {@link Spy} instance.
+     */
+    @AliasFor("target")
+    Class<?> value() default void.class;
+
+    /**
+     * This parameter specifies the target {@link Class} to be injected with a {@link Spy} instance during test execution. The targets can be regular Spring {@link Bean}s or {@link SofaReference} objects. Note that if multiple targets are matched, they will <b>all</b> be injected with the {@link Spy} instance.
+     */
+    @AliasFor("value")
+    Class<?> target() default void.class;
+
+    /**
+     * This optional parameter specifies the {@link SofaReference#uniqueId()} for the target {@link SofaReference} to be injected with a {@link Spy} instance during test execution.
+     */
+    String uniqueId() default "";
+
+    /**
+     * This optional parameter specifies the qualifier (i.e., {@link Bean#name()}) for the target {@link Bean} to be injected with a {@link Spy} instance during test execution.
+     */
+    String qualifier() default "";
+
+    /**
+     * This optional parameter specifies the field name in the targets that will be replaced by the {@link Spy} instance. If this parameter is not specified, the name of the {@link Spy} will be used by default.
+     */
+    String field() default "";
+
+    /**
+     * This parameter specifies the SOFA module to be searched when resolving the stub targets. If this parameter is not specified, all SOFA modules will be searched by default, which may result in increased processing time!
+     */
+    String module() default "";
+
+    @Target({ElementType.FIELD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface SofaSpyBeans {
+        SofaSpyBeanFor[] value();
+    }
+}
---
+++ sofa-boot-project/sofa-boot-core/test-sofa-boot/src/main/java/com/alipay/sofa/testing/model/stub/SofaStub.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.testing.model.stub;
+
+import com.alipay.sofa.testing.resolver.SofaBootBeanResolver;
+import com.alipay.sofa.testing.resolver.SofaBootReferenceResolver;
+import com.google.common.base.Preconditions;
+import lombok.Builder;
+import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.test.context.TestContext;
+import org.springframework.util.Assert;
+
+import javax.annotation.Nullable;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+/**
+ * @author pengym
+ * @version SofaStub.java, v 0.1 2023年08月07日 19:27 pengym
+ */
+@Data
+@Builder
+public class SofaStub implements Stub {
+    private final Class<?> type;
+    @Nullable
+    private final String   moduleName;
+    @Nullable
+    private final String   uniqueId;
+    @Nullable
+    private final String   qualifier;
+
+    @Override
+    public Set<Object> resolveTargets(TestContext testContext) {
+        Assert.notNull(type, "Stub target type is unknown");
+
+        Set<Object> result = new LinkedHashSet<>();
+
+        // 1. Only try to resolve SOFA reference
+        boolean resolveRefOnly = StringUtils.isNotBlank(uniqueId);
+        // 2. Only try to resolve Spring Beans
+        boolean resolveBeanOnly = StringUtils.isNotBlank(moduleName) || StringUtils.isNotBlank(qualifier);
+
+        if (!resolveRefOnly) {
+            // Beans in TestContext
+            result.addAll(SofaBootBeanResolver.findBeans(type, qualifier, testContext.getApplicationContext()));
+            // Beans in SOFA module Context
+            SofaBootBeanResolver resolver = SofaBootBeanResolver.getInstance(testContext);
+            result.addAll(resolver.findModuleBeans(moduleName, type, qualifier));
+        }
+
+        if (!resolveBeanOnly) {
+            init(testContext);
+            // SOFA Reference
+            SofaBootReferenceResolver resolver = SofaBootReferenceResolver.getInstance(testContext);
+            result.add(resolver.reference(type, uniqueId));
+        }
+
+        // Filter null elements
+        result = result.stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+
+        Preconditions.checkState(result.size() > 0,
+                String.format(
+                        "Cannot find SOFA bean/reference with module=[%s], interfaceType=[%s], uniqueId=[%s], qualifier=[%s]",
+                        moduleName, type, uniqueId, qualifier));
+
+        return result;
+    }
+
+    private static AtomicBoolean initialized = new AtomicBoolean(false);
+    private static void init(TestContext testContext) {
+        if (initialized.getAndSet(true)) {
+            return;
+        }
+        testContext.getApplicationContext().getBeansOfType(SofaBootBeanResolver.class);
+    }
+}
