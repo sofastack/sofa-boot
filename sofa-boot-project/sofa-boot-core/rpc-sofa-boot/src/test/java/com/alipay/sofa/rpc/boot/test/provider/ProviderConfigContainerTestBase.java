3b803af1af04885655828b83d70b0479c0327295
HzjNeverStop <441627022@qq.com>
2023-10-23 17:06:09 +0800

    Cherrypick 1254 (#1260)
    * init interface class canonical name to avoid multi reflection
    
    * Support sofa-rpc provider register blacklist/whitelist (#1254)
    
    ---------
    
    Co-authored-by: 致节 <hzj266771@antgroup.com>

---
+++ sofa-boot-project/sofa-boot-core/rpc-sofa-boot/src/test/java/com/alipay/sofa/rpc/boot/test/provider/ProviderConfigContainerTestBase.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.rpc.boot.test.provider;
+
+import com.alipay.sofa.rpc.boot.container.ProviderConfigContainer;
+import com.alipay.sofa.rpc.boot.test.bean.SampleFacade;
+import com.alipay.sofa.rpc.boot.test.bean.SampleFacadeImpl;
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.alipay.sofa.runtime.api.annotation.SofaReferenceBinding;
+import com.alipay.sofa.runtime.api.annotation.SofaService;
+import com.alipay.sofa.runtime.api.annotation.SofaServiceBinding;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author huzijie
+ * @version ProviderConfigContainerTests.java, v 0.1 2023年09月27日 11:14 AM huzijie Exp $
+ */
+@SpringBootApplication
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Import(ProviderConfigContainerTestBase.RpcPublishConfiguration.class)
+public class ProviderConfigContainerTestBase {
+
+    @SofaReference(jvmFirst = false, binding = @SofaReferenceBinding(bindingType = "bolt"))
+    protected SampleFacade          sampleFacadeA;
+
+    @SofaReference(jvmFirst = false, binding = @SofaReferenceBinding(bindingType = "bolt"), uniqueId = "uniqueId")
+    protected SampleFacade          sampleFacadeB;
+
+    @Autowired
+    private ProviderConfigContainer providerConfigContainer;
+
+    @After
+    public void clearExported() {
+        providerConfigContainer.unExportAllProviderConfig();
+    }
+
+    @Configuration
+    static class RpcPublishConfiguration {
+
+        @SofaService(bindings = { @SofaServiceBinding(bindingType = "bolt") })
+        @Bean
+        public SampleFacade providerA() {
+            return new SampleFacadeImpl();
+        }
+
+        @SofaService(bindings = { @SofaServiceBinding(bindingType = "bolt") }, uniqueId = "uniqueId")
+        @Bean
+        public SampleFacade providerB() {
+            return new SampleFacadeImpl();
+        }
+    }
+
+}
